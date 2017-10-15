0b3a8f8c9c71a99402c38f1e2f7441cc2f520f93
Christopher Landry <herbie1982@outlook.com>
2017-10-15 08:47:12 -0400

    added database table for image request history

--- db/postgresqlstartup.sh
+++ db/postgresqlstartup.sh
@@ -7,6 +7,7 @@
 # Swap placeholder values with environment vars
 sqlcmd_createuser="CREATE USER $PGSQL_DB_USERNAME WITH NOCREATEDB NOCREATEROLE NOSUPERUSER ENCRYPTED PASSWORD '$PGSQL_DB_USER_PASSWORD';"
 sqlcmd_createdb="CREATE DATABASE $PGSQL_DB_NAME WITH OWNER $PGSQL_DB_USERNAME;"
+sqlcmd_createtable="CREATE TABLE image (id varchar(10) PRIMARY KEY, url text, source_url text);"
 
 # Run the database setup script
 /usr/bin/pg_ctl start -D /var/lib/pgsql/data
@@ -14,6 +15,8 @@ sleep 10
 
 psql --command "$sqlcmd_createuser"
 psql --command "$sqlcmd_createdb"
+export PGPASSWORD=$PGSQL_DB_USER_PASSWORD
+psql $PGSQL_DB_NAME -U $PGSQL_DB_USERNAME --command "$sqlcmd_createtable" -w
 
 # So that others can't get this value so easily
 unset PGSQL_DB_USER_PASSWORD
